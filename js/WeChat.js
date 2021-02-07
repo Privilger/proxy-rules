9c5582ba666ce0c577d6f93a8e4f59c724a8a72f
Yizheng Zhang <zhangyizheng1996@outlook.com>
2021-02-07 16:20:34 +0800

    Update WeChat.js

--- js/WeChat.js
+++ js/WeChat.js
@@ -1,6 +1,6 @@
 /*	
 [Script]	
-http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://Choler.github.io/Surge/Script/WeChat.js	
+http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://raw.githubusercontent.com/Privilger/proxy-rules/master/js/WeChat.js	
 [MITM]	
 hostname = mp.weixin.qq.com	
 */	
